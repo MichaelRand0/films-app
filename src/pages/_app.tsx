5c01c94748a236d79798bbbecbdae4b125fcaabb
MichaelRand0 <mihailstepanov.stepanoff2015@yandex.ru>
2023-04-26 14:26:09 +0300

    :heavy_plus_sign: Added Redux toolkit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import '@/styles/globals.css'
-import { ThemeProvider } from '@material-tailwind/react'
 import type { AppProps } from 'next/app'
+import { store } from '../../store'
+import { Provider } from 'react-redux'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    
+    <Provider store={store}>
       <Component {...pageProps} />
-
+    </Provider>
   )
 }
